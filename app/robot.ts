f4f199bf0087e336590af3e20fc248cbe56fa81d
Meldziuk Artem <retrosexyal@gmail.com>
2026-01-21 18:48:52 +0300

    feat:sitemap

--- app/robot.ts
+++ app/robot.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = process.env.SITE_URL || "https://example.com";
+  const siteUrl = process.env.SITE_URL;
   return {
     rules: [
       {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,53 @@
 import type { MetadataRoute } from "next";
+import fs from "fs";
+import path from "path";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = process.env.SITE_URL || "https://example.com";
+  const siteUrl = process.env.SITE_URL!;
   const now = new Date();
 
-  const routes = [
-    "/",
-    "/convert",
-    "/webp-to-png",
-    "/png-to-jpeg",
-    "/jpeg-to-webp",
-  ];
+  const appDir = path.join(process.cwd(), "app");
 
-  return routes.map((path) => ({
-    url: `${siteUrl}${path}`,
+  // Берём эталонные страницы из (ru)
+  const rootDir = path.join(appDir, "(ru)");
+
+  const basePages = fs
+    .readdirSync(rootDir, { withFileTypes: true })
+    .filter((d) => d.isDirectory())
+    .map((d) => d.name);
+
+  // Языки
+  const locales = fs
+    .readdirSync(appDir, { withFileTypes: true })
+    .filter(
+      (d) =>
+        d.isDirectory() &&
+        !d.name.startsWith("(") &&
+        !["api", "components"].includes(d.name)
+    )
+    .map((d) => d.name);
+
+  const routes: string[] = [];
+
+  // корневые
+  for (const page of basePages) {
+    routes.push(`/${page}`);
+  }
+
+  // локализованные
+  for (const locale of locales) {
+    for (const page of basePages) {
+      routes.push(`/${locale}/${page}`);
+    }
+  }
+
+  // главная
+  routes.push("/");
+
+  return routes.map((route) => ({
+    url: `${siteUrl}${route}`,
     lastModified: now,
     changeFrequency: "weekly",
-    priority: path === "/" ? 1 : 0.8,
+    priority: route === "/" ? 1 : 0.8,
   }));
 }
